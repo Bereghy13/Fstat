00e366167e8c65f9f2b361e666c62477533f0ab0
bereghy13 <bereghy13@gmail.com>
2023-01-06 16:47:50 +0100

    RESULT SORT

--- RESULT_SORT.py
+++ RESULT_SORT.py
@@ -9,7 +9,10 @@ def CSV_BTTS_SORT(csv_reader):
     line_count = 0
     btts_pred_num = 0
     btts_res_num = 0
+    csv_lenght = 0
     BTTS_LIST = []
+    BTTS_sum = 0
+    last_evaluation = False
 
     def BTTS_YES(btts_pred_num, line_count, btts_res_num):
         btts_pred_num += 1
@@ -22,21 +25,53 @@ def CSV_BTTS_SORT(csv_reader):
                 f'BTTS Predictions: {row["BTTS Average"]}\n\n')
         return btts_pred_num, line_count, btts_res_num
 
+    def BTTS_NO(btts_pred_num, line_count, btts_res_num):
+        btts_pred_num += 1
+        if int(row["Result - Home Team Goals"]) < 1 or int(row["Result - Away Team Goals"]) < 1:
+            line_count += 1
+            btts_res_num += 1
+            BTTS_LIST.append(
+                f'{line_count}. {row["date_GMT"]} - {row["Country"]}: {row["League"]} ==> {row["Home Team"]} - {row["Away Team"]} -'
+                f' {row["Result - Home Team Goals"]}:{row["Result - Away Team Goals"]} - {row["Match Status"]} - '
+                f'BTTS Predictions: {row["BTTS Average"]}\n\n')
+        return btts_pred_num, line_count, btts_res_num
+
+    #def BTTS_eval():
+        # if btts_pred_num > 0:
+        #     print(f'BTTS eseményszám a {BTTS_Avg_Low} - {BTTS_Avg_Low + BTTS_interval} tartományban: {btts_pred_num} - Bekövetkezett BTTS eseményszám: {btts_res_num} - A bekövetkezés aránya: {(btts_res_num / btts_pred_num) * 100}')
+        #     # print(''.join(BTTS_LIST))
+        #     BTTS_sum += btts_pred_num
+        #     BTTS_interval = 10
+        #     BTTS_Avg_Low = BTTS_Avg_Low - BTTS_interval
+        #     btts_pred_num = 0
+        #     btts_res_num = 0
+        #     BTTS_LIST.clear()
+        # else:
+        #     print(f'Nincs meghatározott esemény a {BTTS_Avg_Low} és a {BTTS_Avg_Low + BTTS_interval} tartományban!')
+        #     BTTS_interval = 10
+        #     BTTS_Avg_Low = BTTS_Avg_Low - BTTS_interval
+
     sorted_csv = sorted(csv_reader, key=lambda row: int(row["BTTS Average"]), reverse=True)
 
+    print(f'CSV file sorainak száma: {len(sorted_csv)}')
+
     for row in sorted_csv:
+        csv_lenght += 1
+
         if row["Match Status"] == "complete":
             if BTTS_Avg_Low > int(row["BTTS Average"]):
                 if btts_pred_num > 0:
                     print(f'BTTS eseményszám a {BTTS_Avg_Low} - {BTTS_Avg_Low + BTTS_interval} tartományban: {btts_pred_num} - Bekövetkezett BTTS eseményszám: {btts_res_num} - A bekövetkezés aránya: {(btts_res_num / btts_pred_num) * 100}')
-                    #print(''.join(BTTS_LIST))
+                    # print(''.join(BTTS_LIST))
+                    BTTS_sum += btts_pred_num
                     BTTS_interval = 10
                     BTTS_Avg_Low = BTTS_Avg_Low - BTTS_interval
                     btts_pred_num = 0
                     btts_res_num = 0
                     BTTS_LIST.clear()
                 else:
-                    print(f'Nincs meghatározott esemény a {BTTS_Avg_Low} és a {BTTS_Avg_Low + BTTS_interval} tartományban!')
+                    print(
+                        f'Nincs meghatározott esemény a {BTTS_Avg_Low} és a {BTTS_Avg_Low + BTTS_interval} tartományban!')
                     BTTS_interval = 10
                     BTTS_Avg_Low = BTTS_Avg_Low - BTTS_interval
 
@@ -52,28 +87,34 @@ def CSV_BTTS_SORT(csv_reader):
                 case 50:
                     btts_pred_num, line_count, btts_res_num = BTTS_YES(btts_pred_num, line_count, btts_res_num)
                 case 40:
-                    btts_pred_num, line_count, btts_res_num = BTTS_YES(btts_pred_num, line_count, btts_res_num)
+                    btts_pred_num, line_count, btts_res_num = BTTS_NO(btts_pred_num, line_count, btts_res_num)
                 case 30:
-                    btts_pred_num, line_count, btts_res_num = BTTS_YES(btts_pred_num, line_count, btts_res_num)
+                    btts_pred_num, line_count, btts_res_num = BTTS_NO(btts_pred_num, line_count, btts_res_num)
                 case 20:
-                    btts_pred_num, line_count, btts_res_num = BTTS_YES(btts_pred_num, line_count, btts_res_num)
+                    btts_pred_num, line_count, btts_res_num = BTTS_NO(btts_pred_num, line_count, btts_res_num)
                 case 10:
-                    btts_pred_num, line_count, btts_res_num = BTTS_YES(btts_pred_num, line_count, btts_res_num)
+                    btts_pred_num, line_count, btts_res_num = BTTS_NO(btts_pred_num, line_count, btts_res_num)
                 case 0:
-                    btts_pred_num, line_count, btts_res_num = BTTS_YES(btts_pred_num, line_count, btts_res_num)
+                    btts_pred_num, line_count, btts_res_num = BTTS_NO(btts_pred_num, line_count, btts_res_num)
 
+            if csv_lenght == len(sorted_csv):
+                if btts_pred_num > 0:
+                    print(f'BTTS eseményszám a {BTTS_Avg_Low} - {BTTS_Avg_Low + BTTS_interval} tartományban: {btts_pred_num} - Bekövetkezett BTTS eseményszám: {btts_res_num} - A bekövetkezés aránya: {(btts_res_num / btts_pred_num) * 100}')
+                    BTTS_sum += btts_pred_num
+                else:
+                    print(f'Nincs meghatározott esemény a {BTTS_Avg_Low} és a {BTTS_Avg_Low + BTTS_interval} tartományban!')
 
+    print(f'A BTTS események száma: {BTTS_sum}')
 
 start = time.time()
 
-filename = "sample01.csv"
+filename = "sample.csv"
 
 print(f'Első file felolvasása: {filename}; File mérete: {os.path.getsize(filename)}; Utolsó módosítás dátuma: {datetime.datetime.fromtimestamp(os.path.getmtime(filename))}')
 
 with open(filename, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     CSV_BTTS_SORT(csv_reader)
-
 
 end = time.time()
 
